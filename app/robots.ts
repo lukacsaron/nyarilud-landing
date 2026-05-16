2dc27cbd44b8f1b03918cc9c503937240ceeb14d
Aron Lukacs <aron.lukacs@gmail.com>
2026-05-16 18:35:41 +0200

    update robots.txt rules to allow access for specific user agents including Facebook, Meta, and Twitter

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,12 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [
+      { userAgent: "*", allow: "/" },
+      { userAgent: "facebookexternalhit", allow: "/" },
+      { userAgent: "meta-externalagent", allow: "/" },
+      { userAgent: "Twitterbot", allow: "/" },
+    ],
     sitemap: "https://nyarilud.hu/sitemap.xml",
     host: "https://nyarilud.hu",
   };
